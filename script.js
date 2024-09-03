ef0d307efff2554c8745b8a99ed9ecdc50c95f16
user.name" "MAITRI PATEL <user.email: "maitricode@gmail.com>
2024-09-03 17:48:31 +0530

    message

---
+++ script.js
@@ -0,0 +1,40 @@
+function navAnimation(){
+    let nav = document.querySelector("nav")
+
+nav.addEventListener("mouseenter",function(){
+    let tl = gsap.timeline()
+
+    tl.to(".nav-bottom",{
+        height:"23vh",
+    })
+    tl.to(".nav-part2 h5",{
+        display:"block",
+    })
+    tl.to(".nav-part2 h5 span",{
+        y:0,
+        stagger:{
+            amount:0.6
+        }
+    })
+})
+
+nav.addEventListener("mouseleave",function(){
+    let tl = gsap.timeline()
+    tl.to(".nav-part2 h5 span",{
+        y:25,
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to(".nav-part2 h5",{
+        display:"none",
+        duration:0.1
+    })
+    tl.to(".nav-bottom",{ 
+        height:"0vh",
+        duration:0.2
+    })
+})
+}
+
+navAnimation();
