bb21b437a38397c3bfc83147fe8c65b8d742fac1
user.name" "MAITRI PATEL <user.email: "maitricode@gmail.com>
2025-02-26 08:19:40 +0530

    new commit

--- script.js
+++ script.js
@@ -1,40 +1,107 @@
-function navAnimation(){
+function navAnimation() {
     let nav = document.querySelector("nav")
 
-nav.addEventListener("mouseenter",function(){
-    let tl = gsap.timeline()
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
 
-    tl.to(".nav-bottom",{
-        height:"23vh",
+        tl.to(".nav-bottom", {
+            height: "23vh",
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block",
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: 0,
+            stagger: {
+                amount: 0.6
+            }
+        })
     })
-    tl.to(".nav-part2 h5",{
-        display:"block",
-    })
-    tl.to(".nav-part2 h5 span",{
-        y:0,
-        stagger:{
-            amount:0.6
-        }
-    })
-})
 
-nav.addEventListener("mouseleave",function(){
-    let tl = gsap.timeline()
-    tl.to(".nav-part2 h5 span",{
-        y:25,
-        stagger:{
-            amount:0.2
-        }
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+        tl.to(".nav-part2 h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.2
+            }
+        })
+        tl.to(".nav-part2 h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to(".nav-bottom", {
+            height: "0vh",
+            duration: 0.2
+        })
     })
-    tl.to(".nav-part2 h5",{
-        display:"none",
-        duration:0.1
+}
+
+
+
+// for page 2
+
+function page2Animation() {
+    let rightElem = document.querySelectorAll(".right-elem");
+
+    rightElem.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1,
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0,
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 50,
+                y: dets.y - elem.getBoundingClientRect().y - 130,
+            })
+        })
     })
-    tl.to(".nav-bottom",{ 
-        height:"0vh",
-        duration:0.2
+}
+
+function page3Animation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
     })
-})
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
 }
 
 navAnimation();
+page2Animation();
+page3Animation();
+
+let sections = document.querySelectorAll(".sec-right");
+
+sections.forEach(function (elem) {
+    elem.addEventListener('mouseenter', function () {
+        elem.childNodes[3].style.opacity = 1;
+        elem.childNodes[3].play();
+    })
+
+    elem.addEventListener('mouseleave', function () {
+        elem.childNodes[3].style.opacity = 0;
+        elem.childNodes[3].load();
+    })
+}) 
